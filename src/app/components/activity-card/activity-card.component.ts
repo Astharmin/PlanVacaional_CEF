0745509a72143c2a24c267108918e9010f6bc44a
Aaron Barcenas <salgadobarcenas96@gmail.com>
2025-07-09 15:00:54 -0400

    arreglos y justes

---
+++ src/app/components/activity-card/activity-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-card',
+  imports: [],
+  template: `
+    <p>
+      activity-card works!
+    </p>
+  `,
+  styles: ``
+})
+export class ActivityCardComponent {
+
+}
